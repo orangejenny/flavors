fab7fe5d2ab75133326c10d2635b7ce4d0d104c3
Jenny Schweers <orange.jenny@gmail.com>
2015-01-31 13:49:16 -0500

    Clean up header a bit on songs page

--- public/javascript/songs.js
+++ public/javascript/songs.js
@@ -252,33 +252,36 @@ function ratingHTML(iconClass, number) {
 
 function refreshTable() {
 	var $table = jQuery("#song-table-container");
-	var $cells = $table.find("tr:not(:first)").find("td.rating, td:last-child");
-	$cells.attr("contenteditable", "true");
+	$table.find("tr:not(:first)").find("td.rating, td:last-child").attr("contenteditable", "true");
 
 	var tableWidth = $table.width();
 	var $cells = $table.find("tr:visible:first td");
-	var $filters = jQuery("#simple-filters").find("input");
 	var cellWidths = [undefined, .02, .15, .15, .15, .05, .05, .05, .38];
-	var filterWidths = [undefined, undefined, .15, .15, .15, undefined, undefined, undefined, .25];
-	var filterIndex = 0;
-	var leftMargin = 0;
 	for (var i = 1; i < cellWidths.length; i++) {
 		jQuery($cells[i]).css("width", Math.round(cellWidths[i] * tableWidth) + "px");
-		/*if (filterWidths[i]) {
-			jQuery($filters[filterIndex]).css("float", "left");
-			jQuery($filters[filterIndex]).css("width", Math.round(filterWidths[i] * tableWidth) + "px");
-			//jQuery($filters[filterIndex]).css("leftMargin", leftMargin + "px");
-			filterIndex++;
-			leftMargin = 0;
-		}
-		else {
-			leftMargin += cellWidths[i] * screenWidth;
-		}*/
 	}
 
+	// Align filter inputs with columns, which vary. Terrible.
 	var $filters = jQuery('#simple-filters');
 	var placeholders = ["name", "artist", "collections", "tags"];
+	var cells = {};
+	$table.find("tr:first td").each(function(i) {
+		cells[jQuery(this).text().trim().toLowerCase()] = i + 1;
+	});
+	var $firstRow = $table.find("tr:visible:first");
 	$filters.find("input").each(function() {
-		jQuery(this).get(0).placeholder = placeholders.shift();
+		var placeholder = placeholders.shift();
+		var $input = jQuery(this);
+		var $cell = $firstRow.find("td:nth-child(" + cells[placeholder] + ")");
+		$input.width($cell.width() - parseInt($input.css("margin-left")) - parseInt($input.css("margin-right")));
+		var left = 0;
+		for (var i = 1; i < cells[placeholder]; i++) {
+			var $cell = $firstRow.find("td:nth-child(" + i + "):visible");
+			if ($cell.length) {
+				left += $cell.width() + 5;
+			}
+		}
+		$input.css("left", left + "px");
+		$input.get(0).placeholder = placeholder;
 	});
 }

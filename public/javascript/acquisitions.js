e16eed04a429cdd3e821ea6c0ddf2209a46d7eaa
Jenny Schweers <orange.jenny@gmail.com>
2015-09-20 19:36:42 -0400

    cleanup

--- public/javascript/acquisitions.js
+++ public/javascript/acquisitions.js
@@ -9,14 +9,21 @@ jQuery(document).ready(function() {
 	});
 });
 
-function AcquisitionsChart(containerSelector) {
+function AcquisitionsChart(selector) {
 	var self = this;
-	self.containerSelector = containerSelector;
-	self.width = jQuery(containerSelector).width();
-	self.height = 400;
-	self.xAxisMargin = 20;
 	self.barMargin = 0;
-	self.svg = d3.select(self.containerSelector + " svg");
+	self.selector = selector;
+	self.svg = d3.select(self.selector + " svg");
+	self.width = jQuery(selector).width();
+	self.height = 400;
+	self.xAxis = d3.svg.axis().orient('bottom');
+	self.xAxisMargin = 20;
+};
+
+AcquisitionsChart.prototype.attachEvents = function() {
+	var self = this;
+	attachSelectionHandlers(self.selector + " g");
+	attachTooltip(self.selector + " g:not(.axis)");
 };
 
 AcquisitionsChart.prototype.draw = function(data) {
@@ -28,93 +35,105 @@ AcquisitionsChart.prototype.draw = function(data) {
 	self.attachEvents();
 };
 
-AcquisitionsChart.prototype.setDimensions = function() {
-	this.svg.attr("width", this.width)
-			  .attr("height", this.height);
+AcquisitionsChart.prototype.drawBars = function(data, dateStrings) {
+	var self = this;
+	var barData = self.getBarData(data, dateStrings);
+	var barSize = self.getBarSize(self.getMinMonthCount(dateStrings), self.getMaxMonthCount(dateStrings));
+	var bars = self.svg.selectAll("g")
+							.data(barData)
+							.enter().append("g")
+							.attr("transform", function(d, i) {
+								return "translate(" + d.monthCount * barSize + ", 0)";
+							});
+
+	var scale = self.getScale(barData);
+	bars.append("rect")
+			.attr("y", function(d) { return self.height - self.xAxisMargin - scale(d.count); })
+			.attr("width", barSize - self.barMargin)
+			.attr("height", function(d) { return scale(d.count); });
 };
 
-AcquisitionsChart.prototype.barSize = function(minMonthCount, maxMonthCount) {
+AcquisitionsChart.prototype.drawAxes = function(dateStrings) {
+	var self = this;
+	self.drawXAxis();
+	self.formatXAxis(dateStrings);
+	self.drawXAxisLabels();
+};
+
+AcquisitionsChart.prototype.drawXAxis = function(dateStrings) {
+	var self = this;
+	self.svg.append("line")
+			.attr("class", "axis")
+			.attr("x1", 0)
+			.attr("y1", self.height - self.xAxisMargin)
+			.attr("x2", self.width)
+			.attr("y2", self.height - self.xAxisMargin);
+};
+
+AcquisitionsChart.prototype.drawXAxisLabels = function(dateStrings) {
+	var self = this;
+	self.svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(0," + (self.height - self.xAxisMargin) + ")")
+			.call(self.xAxis);
+	self.svg.selectAll(".axis text").attr("y", 2);
+};
+
+AcquisitionsChart.prototype.formatXAxis = function(dateStrings) {
+	var self = this;
+	var barSize = self.getBarSize(self.getMinMonthCount(dateStrings), self.getMaxMonthCount(dateStrings));
+	self.xAxis.tickValues(_.map(_.range(0, self.width, barSize * 12), function(x) { return x + barSize * 6; }))
+			.tickFormat(function(t) { return Math.round((t - barSize * 6) / barSize) / 12 + self.getMinYear(dateStrings); });
+};
+
+AcquisitionsChart.prototype.getBarData = function(data, dateStrings) {
+	var self = this;
+	var dateFormat = d3.time.format("%b %Y");
+	return _.map(data, function(d) {
+		var date = new Date(d.DATESTRING + "-15");
+		var text = dateFormat(date);
+		return {
+			date: date,
+			month: date.getMonth() + 1,
+			year: date.getFullYear(),
+			monthCount: date.getFullYear() * 12 + date.getMonth() - self.getMinMonthCount(dateStrings),
+			count: +d.COUNT,
+			condition: "extract(month from mindateacquired) = " + (date.getMonth() + 1) + " and extract(year from mindateacquired) = " + date.getFullYear(),
+			filename: "acquired " + text,
+			description: text + "\n" + d.COUNT + Pluralize(+d.COUNT, " collection"),
+			samples: d.SAMPLES,
+		};
+	});
+};
+
+AcquisitionsChart.prototype.getBarSize = function(minMonthCount, maxMonthCount) {
 	var self = this;
 	return self.width / (maxMonthCount - minMonthCount + 1) - self.barMargin;
 };
 
-AcquisitionsChart.prototype.scale = function(data) {
+AcquisitionsChart.prototype.getMaxMonthCount = function(dateStrings) {
+	var maxDate = new Date(d3.max(dateStrings) + "-15");
+	return maxDate.getFullYear() * 12 + 12;
+};
+
+AcquisitionsChart.prototype.getMinMonthCount = function(dateStrings) {
+	var minDate = new Date(d3.min(dateStrings) + "-15");
+	return minDate.getFullYear() * 12;
+};
+
+AcquisitionsChart.prototype.getMinYear = function(dateStrings) {
+	var minDate = new Date(d3.min(dateStrings) + "-15");
+	return minDate.getFullYear();
+};
+
+AcquisitionsChart.prototype.getScale = function(data) {
 	var self = this;
 	var scale = d3.scale.linear().range([0, self.height - self.xAxisMargin]);
 	scale.domain([0, d3.max(_.pluck(data, 'count'))]);
 	return scale;
 };
 
-AcquisitionsChart.prototype.minMonthCount = function(dateStrings) {
-	var minDate = new Date(d3.min(dateStrings) + "-15");
-	return minDate.getFullYear() * 12;
-};
-
-AcquisitionsChart.prototype.maxMonthCount = function(dateStrings) {
-	var maxDate = new Date(d3.max(dateStrings) + "-15");
-	return maxDate.getFullYear() * 12 + 12;
-};
-
-AcquisitionsChart.prototype.minYear = function(dateStrings) {
-	var minDate = new Date(d3.min(dateStrings) + "-15");
-	return minDate.getFullYear();
-};
-
-AcquisitionsChart.prototype.drawBars = function(data, dateStrings) {
-	var self = this;
-	var dateFormat = d3.time.format("%b %Y");
-			data = _.map(data, function(d) {
-				var date = new Date(d.DATESTRING + "-15");
-				var text = dateFormat(date);
-				return {
-					date: date,
-					month: date.getMonth() + 1,
-					year: date.getFullYear(),
-					monthCount: date.getFullYear() * 12 + date.getMonth() - self.minMonthCount(dateStrings),
-					count: +d.COUNT,
-					condition: "extract(month from mindateacquired) = " + (date.getMonth() + 1) + " and extract(year from mindateacquired) = " + date.getFullYear(),
-					filename: "acquired " + text,
-					description: text + "\n" + d.COUNT + Pluralize(+d.COUNT, " collection"),
-					samples: d.SAMPLES,
-				};
-			});
-			var barSize = self.barSize(self.minMonthCount(dateStrings), self.maxMonthCount(dateStrings));
-			var bars = self.svg.selectAll("g")
-									.data(data)
-									.enter().append("g")
-									.attr("transform", function(d, i) {
-										return "translate(" + d.monthCount * barSize + ", 0)";
-									});
-
-			var scale = self.scale(data);
-			bars.append("rect")
-					.attr("y", function(d) { return self.height - self.xAxisMargin - scale(d.count); })
-					.attr("width", barSize - self.barMargin)
-					.attr("height", function(d) { return scale(d.count); });
-};
-
-AcquisitionsChart.prototype.drawAxes = function(dateStrings) {
-	var self = this;
-			self.svg.append("line")
-					.attr("class", "axis")
-					.attr("x1", 0)
-					.attr("y1", self.height - self.xAxisMargin)
-					.attr("x2", self.width)
-					.attr("y2", self.height - self.xAxisMargin);
-			var xAxis = d3.svg.axis()
-									.orient('bottom');
-			var barSize = self.barSize(self.minMonthCount(dateStrings), self.maxMonthCount(dateStrings));
-			xAxis.tickValues(_.map(_.range(0, self.width, barSize * 12), function(x) { return x + barSize * 6; }))
-					.tickFormat(function(t) { return Math.round((t - barSize * 6) / barSize) / 12 + self.minYear(dateStrings); });
-			self.svg.append("g")
-					.attr("class", "axis")
-					.attr("transform", "translate(0," + (self.height - self.xAxisMargin) + ")")
-					.call(xAxis);
-			self.svg.selectAll(".axis text").attr("y", 2);
-};
-
-AcquisitionsChart.prototype.attachEvents = function() {
-	var self = this;
-	attachSelectionHandlers(self.containerSelector + " g");
-	attachTooltip(self.containerSelector + " g:not(.axis)");
+AcquisitionsChart.prototype.setDimensions = function() {
+	this.svg.attr("width", this.width)
+			  .attr("height", this.height);
 };

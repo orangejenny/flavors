87bd9a65ef33be7082f15d96ead9b7a5803904d2
Jenny Schweers <orange.jenny@gmail.com>
2015-09-26 12:40:58 -0400

    split out category chart functions

--- public/javascript/facet.js
+++ public/javascript/facet.js
@@ -7,16 +7,29 @@ jQuery(document).ready(function() {
 		SPINNER: selector,
 		FINISH: function(data) {
 			(new Histogram(".histogram-container", facet, 5)).draw(data);
-			(new BinaryChart(".binary-container", facet)).draw(data);
+			(new BinaryHorizontalStack(".binary-container", facet)).draw(data);
 		},
 	});
 
 	// Handler for generating category charts
 	jQuery(".category-buttons button").click(function() {
-		var args = { CATEGORY: jQuery(this).text() };
+		var selector = ".category-container";
+		var category = jQuery(this).text();
 		jQuery(".category-container").each(function() {
-			args.FACET = jQuery(this).data("facet");
-			generateCategoryCharts(args);
+			jQuery(".category-buttons .active").removeClass("active");
+			jQuery(".category-buttons button[data-category='" + category + "']").addClass("active");
+			var facet = jQuery(this).data("facet");
+			CallRemote({
+				SUB: 'Flavors::Data::Tag::CategoryStats',
+				ARGS: {
+					FACET: facet,
+					CATEGORY: category,
+				},
+				SPINNER: selector,
+				FINISH: function(data) {
+					(new CategoryChart(selector, facet, 5)).draw(data);
+				},
+			});
 		});
 	});
 });
@@ -27,111 +40,135 @@ var icons = {
 	mood: 'glyphicon-heart',
 };
 
-function generateCategoryCharts(args) {
-	var category = args.CATEGORY;
-	var facet = args.FACET;
-
-	jQuery(".category-buttons .active").removeClass("active");
-	jQuery(".category-buttons button[data-category='" + category + "']").addClass("active");
-
-	var containerSelector = ".category-container[data-facet='" + facet + "']";
-	var chartSelector = containerSelector + " svg";
-	var width = jQuery(containerSelector).width();
-	var barSize = 20;
-	var textHeight = 13;
-
-	jQuery(chartSelector).html("");
-	var chart = d3.select(chartSelector)
-						.attr("width", width);
-
-	var xScale = d3.scale.linear().range([0, width]);
-
-	CallRemote({
-		SUB: 'Flavors::Data::Tag::CategoryStats',
-		ARGS: args,
-		SPINNER: containerSelector,
-		FINISH: function(data) {
-			data = _.map(data, function(d) { return {
-				tag: d.TAG,
-				rating: +d.RATING,
-				count: +d.COUNT,
-				description: d.COUNT + ' ' + StringMultiply("<span class='glyphicon " + icons[facet] + "'></span>", +d.RATING),
-				condition: "exists (select 1 from songtag where songid = songs.id and tag = '" + d.TAG + "') and " + facet + " = " + d.RATING,
-				filename: '[' + d.TAG + '] ' + facet + ' ' + d.RATING,
-				samples: d.SAMPLES,
-			}; });
-
-			var tagCounts = _.map(_.groupBy(data, function(d) { return d.tag; }), function(d, tag) {
-				var counts = [0, 0, 0, 0, 0];
-				_.each(d, function(x) { counts[x.rating - 1] = +x.count });
-				return { tag: tag, counts: counts };
-			});
-			xScale.domain([0, 2 * _.max(_.map(tagCounts, function(d) {
-				return d.counts[2] / 2 + Math.max(d.counts[0] + d.counts[1], d.counts[3] + d.counts[4]);
-			}))]);
-
-			chart.attr("height", tagCounts.length * (barSize + textHeight));
-			var bars = chart.selectAll("g")
-									.data(data)
-									.enter().append("g")
-									.attr("transform", function(d) {
-										return "translate(0, " + _.findIndex(tagCounts, function(x) { return x.tag === d.tag; }) * (barSize + textHeight) + ")";
-									});
-
-			bars.append("rect")
-					.attr("height", barSize - 5)
-					.attr("width", function(d) { return xScale(d.count); })
-					.attr("x", function(d) {
-						var counts = _.find(tagCounts, function(x) { return x.tag === d.tag; }).counts
-
-						// Start at midpoint
-						var x = width / 2;
-						var direction = d.rating > 3 ? 1 : -1;
-						x += direction * xScale(counts[2] / 2);
-						if (d.rating === 3) {
-							// Center the 3-star rating
-							return x;
-						}
-						if (d.rating < 3) {
-							// Push 1 and 2-star ratings left of center
-							_.each([0, 1], function(i) {
-								if (i >= d.rating - 1) {
-									x -= xScale(counts[i]);
-								}
-							});
-						}
-						else {
-							// Push 4 and 5-star ratings right of center
-							 _.each([3, 4], function(i) {
-								if (i <= d.rating - 1) {
-									x += xScale(counts[i]);
-								}
-							});
-							x -= xScale(d.count);
-						}
-						return x;
-					})
-					.attr("y", textHeight)
-					.attr("class", function(d) { return "rating-" + d.rating; });
-
-			texted = {};
-			bars.each(function(d, i) {
-				if (!texted[d.tag]) {
-					d3.select(this).append("text")
-										.attr("x", width / 2)
-										.attr("y", barSize / 2)
-										.text(function(d) { return d.tag; });
-					texted[d.tag] = 1;
-				}
-			});
-
-			attachTooltip(containerSelector + " g rect");
-			attachSelectionHandlers(containerSelector + " g");
-		},
-	});
+function CategoryChart(selector, facet, range) {
+	var self = this;
+	self.selector = selector;
+	self.facet = facet;
+	self.range = range;
+	self.svg = d3.select(self.selector + " svg");
+	self.width = jQuery(self.selector).width();
+	self.barSize = 20;
+	self.textHeight = 13;
 }
 
-function BinaryChart(selector, facet) {
+CategoryChart.prototype.attachEvents = function() {
+	var self = this;
+	attachTooltip(self.selector + " g rect");
+	attachSelectionHandlers(self.selector + " g");
+};
+
+CategoryChart.prototype.draw = function(data) {
+	var self = this;
+	jQuery(self.selector + " svg").html("");
+	data = self.reformatData(data);
+	self.setDimensions(data);
+	bars = self.drawBars(data);
+	self.drawLabels(bars);
+	self.attachEvents();
+};
+
+CategoryChart.prototype.reformatData = function(data) {
+	return _.map(data, function(d) { return {
+		tag: d.TAG,
+		rating: +d.RATING,
+		count: +d.COUNT,
+		description: d.COUNT + ' ' + StringMultiply("<span class='glyphicon " + icons[self.facet] + "'></span>", +d.RATING),
+		condition: "exists (select 1 from songtag where songid = songs.id and tag = '" + d.TAG + "') and " + self.facet + " = " + d.RATING,
+		filename: '[' + d.TAG + '] ' + self.facet + ' ' + d.RATING,
+		samples: d.SAMPLES,
+	}; });
+};
+
+CategoryChart.prototype.drawBars = function(data) {
+	var self = this;
+	var xScale = d3.scale.linear().range([0, self.width]);
+	xScale.domain([0, 2 * _.max(_.map(self.getTagCounts(data), function(d) {
+		return d.counts[2] / 2 + Math.max(d.counts[0] + d.counts[1], d.counts[3] + d.counts[4]);
+	}))]);
+
+	var bars = self.svg.selectAll("g")
+								.data(data)
+								.enter().append("g")
+								.attr("transform", function(d) {
+									return "translate(0, " + 
+										_.findIndex(self.getTagCounts(data), function(x) { return x.tag === d.tag; }) * (self.barSize + self.textHeight)
+									+ ")";
+								});
+
+	bars.append("rect")
+			.attr("height", self.barSize - 5)
+			.attr("width", function(d) { return xScale(d.count); })
+			.attr("x", function(d) {
+				var counts = _.find(self.getTagCounts(data), function(x) { return x.tag === d.tag; }).counts
+
+				// Start at midpoint
+				var x = self.width / 2;
+				var midRange = self.range / 2;
+				var direction = d.rating > midRange ? 1 : -1;
+				if (midRange !== parseInt(midRange)) {
+					x += direction * xScale(counts[parseInt(midRange)] / 2);
+				}
+				if (d.rating === midRange) {
+					return x;
+				}
+				if (d.rating < midRange) {
+					// Push low ratings left of center
+					_.each(_.range(parseInt(midRange)), function(i) {
+						if (i >= d.rating - 1) {
+							x -= xScale(counts[i]);
+						}
+					});
+				}
+				else {
+					// Push high ratings right of center
+					 _.each(_.map(_.range(parseInt(midRange)), function(x) { return x + Math.ceil(midRange); }), function(i) {
+						if (i <= d.rating - 1) {
+							x += xScale(counts[i]);
+						}
+					});
+					x -= xScale(d.count);
+				}
+				return x;
+			})
+			.attr("y", self.textHeight)
+			.attr("class", function(d) { return "rating-" + d.rating; });
+
+	return bars;
+};
+
+CategoryChart.prototype.drawLabels = function(bars) {
+	var self = this;
+	texted = {};
+	bars.each(function(d, i) {
+		if (!texted[d.tag]) {
+			d3.select(this).append("text")
+								.attr("x", self.width / 2)
+								.attr("y", self.barSize / 2)
+								.text(function(d) { return d.tag; });
+			texted[d.tag] = 1;
+		}
+	});
+};
+
+CategoryChart.prototype.getTagCounts = function(data) {
+	var self = this;
+	if (!self.tagCounts) {
+		self.tagCounts = _.map(_.groupBy(data, function(d) { return d.tag; }), function(d, tag) {
+			var counts = [0, 0, 0, 0, 0];
+			_.each(d, function(x) { counts[x.rating - 1] = +x.count });
+			return { tag: tag, counts: counts };
+		});
+	}
+	return self.tagCounts;
+};
+
+CategoryChart.prototype.setDimensions = function(data) {
+	var self = this;
+	self.svg.attr("width", self.width);
+	self.svg.attr("height", self.getTagCounts(data).length * (self.barSize + self.textHeight));
+};
+
+function BinaryHorizontalStack(selector, facet) {
 	var self = this;
 	self.selector = selector;
 	self.facet = facet;
@@ -142,13 +179,13 @@ function BinaryChart(selector, facet) {
 	self.svg = d3.select(self.selector + " svg");
 }
 
-BinaryChart.prototype.attachEvents = function() {
+BinaryHorizontalStack.prototype.attachEvents = function() {
 	var self = this;
 	attachSelectionHandlers(self.selector + " g");
 	attachTooltip(self.selector + " g");
 }
 
-BinaryChart.prototype.draw = function(data) {
+BinaryHorizontalStack.prototype.draw = function(data) {
 	var self = this;
 	self.setDimensions();
 
@@ -172,7 +209,7 @@ BinaryChart.prototype.draw = function(data) {
 	self.attachEvents();
 };
 
-BinaryChart.prototype.drawBarLabels = function(ratedData, unratedData, bars) {
+BinaryHorizontalStack.prototype.drawBarLabels = function(ratedData, unratedData, bars) {
 	var self = this;
 	bars.append("text")
 			.attr("x", function(d, i) { return i == 0 ? self.barTextOffset : self.width - self.barTextOffset - self.barMargin; })
@@ -181,7 +218,7 @@ BinaryChart.prototype.drawBarLabels = function(ratedData, unratedData, bars) {
 			.text(function(d, i) { return i == 0 ? ratedData.value + " rated" : unratedData.value + " unrated"; });
 };
 
-BinaryChart.prototype.drawBars = function(ratedData, unratedData) {
+BinaryHorizontalStack.prototype.drawBars = function(ratedData, unratedData) {
 	var self = this;
 	var bars = self.svg.selectAll("g")
 								.data([ratedData, unratedData])
@@ -198,7 +235,7 @@ BinaryChart.prototype.drawBars = function(ratedData, unratedData) {
 	return bars;
 };
 
-BinaryChart.prototype.getScale = function(total) {
+BinaryHorizontalStack.prototype.getScale = function(total) {
 	var self = this;
 	var scale = d3.scale.linear();
 	scale.range([0, self.width - self.barMargin]);
@@ -206,7 +243,7 @@ BinaryChart.prototype.getScale = function(total) {
 	return scale;
 };
 
-BinaryChart.prototype.setDimensions = function() {
+BinaryHorizontalStack.prototype.setDimensions = function() {
 	this.svg.attr("width", this.width)
 			  .attr("height", this.height);
 };

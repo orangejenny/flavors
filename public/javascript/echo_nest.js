c8bb18bc23938e6b5e10f96827382d2ace89b4c4
Jenny Schweers <orange.jenny@gmail.com>
2016-02-07 22:15:34 -0500

    functions for showing and hiding error

--- public/javascript/echo_nest.js
+++ public/javascript/echo_nest.js
@@ -1,15 +1,14 @@
 jQuery(document).ready(function() {
-    var $modal = jQuery("#echo-nest");
-    var $table = $modal.find("table");
-    var api_key = $modal.data("api-key");
-    var disambiguationTemplate = _.template(jQuery("#echo-nest-disambiguation-row").text());
-    var summaryTemplate = _.template(jQuery("#echo-nest-summary-row").text());
+    var $modal = jQuery("#echo-nest"),
+        $table = $modal.find("table"),
+        $alert = $modal.find(".alert"),
+        api_key = $modal.data("api-key"),
+        disambiguationTemplate = _.template(jQuery("#echo-nest-disambiguation-row").text()),
+        summaryTemplate = _.template(jQuery("#echo-nest-summary-row").text());
 
     // Click on song table to pop up modal of EchoNest song results
     jQuery(".echo-nest-trigger").on("click", function() {
-        var $alert = $modal.find(".alert");
         var $row = jQuery(this).closest("tr");
-
         var name = $row.find(".name").text();
         var artist = $row.find(".artist").text();
 
@@ -23,6 +22,7 @@ jQuery(document).ready(function() {
                 var $tbody = $table.find("tbody");
                 if (data.response && data.response.songs) {
                     if (data.response.songs.length) {
+                        hideError();
                         $table.removeClass("hide");
                         $tbody.html('');
                         _.each(_.sortBy(data.response.songs, function(song) {
@@ -30,18 +30,15 @@ jQuery(document).ready(function() {
                         }), function(song) {
                             $tbody.append(disambiguationTemplate(song));
                         });
-                        $alert.addClass("hide");
                     } else {
-                        $table.addClass("hide");
-                        $alert.text("No songs found").removeClass("hide");
+                        showError("No songs found");
                     }
                 } else {
-                    $table.addClass("hide");
-                    $alert.text("Error in EchoNest API").removeClass("hide");
+                    showError("Error in EchoNest API");
                 }
             },
             error: function() {
-                $alert.text("Error in EchoNest API").removeClass("hide");
+                showError("Error in EchoNest API");
             },
         });
     });
@@ -55,7 +52,7 @@ jQuery(document).ready(function() {
             url: 'http://developer.echonest.com/api/v4/song/profile?api_key=' + api_key + '&format=json&bucket=audio_summary&id=' + id,
             success: function(data) {
                 if (data.response && data.response.songs.length && data.response.songs[0].audio_summary) {
-                    $table.removeClass("hide");
+                    hideError();
                     var summary = data.response.songs[0].audio_summary;
                     summary.key = keys[summary.key] + " " + (summary.mode ? "major" : "minor");
                     summary.loudness += " dB";
@@ -69,14 +66,22 @@ jQuery(document).ready(function() {
                         }}), function(pair) { return pair.key; }),
                     }));
                 } else {
-                    $alert.text("Error in EchoNest API").removeClass("hide");
-                    $table.addClass("hide");
+                    showError("Error in EchoNest API");
                 }
             },
             error: function(data) {
-                $table.addClass("hide");
-                $alert.text("Error in EchoNest API").removeClass("hide");
+                showError("Error in EchoNest API");
             },
         });
     });
+
+    function showError(text) {
+        $table.addClass("hide");
+        $alert.text(text).removeClass("hide");
+    }
+
+    function hideError() {
+        $table.removeClass("hide");
+        $alert.addClass("hide");
+    }
 });

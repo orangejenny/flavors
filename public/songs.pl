944b07a630d23ef25e39f877f2bf3ebd448a8acc
Jenny Schweers <orange.jenny@gmail.com>
2016-02-11 22:04:24 -0500

    Pull EchoNest-relaated HTML into its own module

--- public/songs.pl
+++ public/songs.pl
@@ -7,6 +7,7 @@ use Flavors::Data::Playlist;
 use Flavors::Data::Song;
 use Flavors::Data::Tag;
 use Flavors::Data::Util;
+use Flavors::EchoNest;
 use Flavors::HTML;
 use Flavors::Util;
 use JSON qw(to_json);
@@ -159,45 +160,6 @@ print qq{
     </div>
 };
 
-printf(qq{
-    <div id="echo-nest" class="modal" data-api-key="%s">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <div class="pull-right">
-                        <a href="http://the.echonest.com/" class="img-link" target="_blank">
-                            <img src="images/echo_nest.png" />
-                        </a>
-                    </div>
-                    <h4 class="modal-title"></h4>
-                </div>
-                <div class="modal-body">
-                    <div class="alert alert-danger hide"></div>
-                    <table class="table table-striped table-hover">
-                        <tbody></tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-}, Flavors::Util::Config->{echo_nest_api_key});
-
-print qq{
-    <script type="text/template" id="echo-nest-disambiguation-row">
-        <tr class="clickable disambiguation" data-id="<%= id %>">
-            <td><%= artist_name %></td>
-            <td><%= title %></td>
-            <td><%= id %></td>
-        </tr>
-    </script>
-    <script type="text/template" id="echo-nest-summary-row">
-        <% for (var i in pairs) { %>
-            <tr>
-                <td><%= pairs[i].key %></td>
-                <td><%= pairs[i].value %></td>
-            </tr>
-        <% } %>
-    </script>
-};
+print Flavors::EchoNest::ModalHTML();
 
 print Flavors::HTML::Footer();

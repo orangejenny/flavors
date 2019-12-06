0a0e8bd4c302be1c556e6c1457319292afabf44e
Jenny Schweers <orange.jenny@gmail.com>
2019-12-06 18:51:07 -0500

    Removed dead code

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -127,36 +127,6 @@ function AssertArgs(args, required, optional) {
 }
 
 /*
- * BuildArgs
- *
- * Description: Creates an args object based on the inputs found in a given selector.
- * Parameters
- *    selector: CSS selector of container holding the inputs
- *    args: (optional) an object to add the args to
- */
-function BuildArgs(selector, args) {
-    if (!args) {
-        args = {};
-    }
-
-    jQuery(selector + ' select, ' + selector + ' input, ' + selector + ' textarea').each(function() {
-        var name = jQuery(this).attr('name');
-        if (name) {
-            var value;
-            if (jQuery(this).is(':checkbox')) {
-                value = jQuery(this).is(':checked') ? 1 : 0;
-            }
-            else {
-                value = jQuery(this).val();
-            }
-            args[name] = value;
-        }
-    });
-
-    return args;
-}
-
-/*
  * ExportPlaylist
  *
  * Description: Presents user with M3U file of songs to download.
@@ -182,20 +152,6 @@ function ExportPlaylist(args) {
 
     console.log('export.pl?' + params.join('&'));
     document.location = 'export.pl?' + params.join('&');
-}
-
-/*
- * UpperCaseFirst
- *
- * Description: ucfirst
- *
- * Args
- *    String to manipulate.
- */
-function UpperCaseFirst(str) {
-    var first = str.substring(0, 1).toUpperCase();
-    var rest = str.substring(1).toLowerCase();
-    return first + rest;
 }
 
 /*
